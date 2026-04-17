f3b04094bea857549557e0c4aa54a0fa823112f1
Shreyash <shreyashdesai31796@gmail.com>
2026-04-17 19:18:51 +0530

    Models added and Downloaded Required Packages for TaskBoard.Api

---
+++ TaskBoard.Api/Models/ProjectTask.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskBoard.Api.Models
+{
+    public class ProjectTask
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+
+        public Priority Priority { get; set; }
+
+        public Status Status { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public Project Project { get; set; } = null!;
+
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+
+    }
+    public enum Priority 
+    { 
+        Low ,
+        Medium ,
+        High ,
+        Critical
+    }
+
+    public enum Status 
+    { 
+        Todo ,
+        InProgress ,
+        Review ,
+        Done
+    }
+}
