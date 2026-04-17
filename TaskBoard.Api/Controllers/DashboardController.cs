a3112252d67d194622306b3b0f46701a7184c348
Shreyash <shreyashdesai31796@gmail.com>
2026-04-18 00:53:19 +0530

    Updates some controllers

--- TaskBoard.Api/Controllers/DashboardController.cs
+++ TaskBoard.Api/Controllers/DashboardController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskBoard.Api.Data;
+using TaskBoard.Api.Models;
+using System.Linq;
 
 namespace TaskBoard.Api.Controllers
 {
@@ -7,7 +10,41 @@ namespace TaskBoard.Api.Controllers
     [ApiController]
     public class DashboardController : ControllerBase
     {
+        private readonly AppDbContext _context;
 
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
 
+        [HttpGet("{projectId}")]
+        public async Task<IActionResult> GetDashboard(int projectId)
+        {
+            var tasks = await _context.Tasks
+                .Where(t => t.ProjectId == projectId)
+                .ToListAsync();
+
+            var totalTasks = tasks.Count;
+
+            var todo = tasks.Count(t => t.Status == Status.Todo);
+            var inProgress = tasks.Count(t => t.Status == Status.InProgress);
+            var review = tasks.Count(t => t.Status == Status.Review);
+            var done = tasks.Count(t => t.Status == Status.Done);
+
+            var overdue = tasks.Count(t =>
+                t.DueDate.HasValue &&
+                t.DueDate < DateTime.UtcNow &&
+                t.Status != Status.Done);
+
+            return Ok(new
+            {
+                totalTasks,
+                todo,
+                inProgress,
+                review,
+                done,
+                overdue
+            });
+        }
     }
 }
